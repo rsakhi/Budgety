33a86e958dd0e6e8ed905c5f77bf5ec3686a7e4e
rsakhi <sakhi.rohit@gmail.com>
2018-09-30 17:41:44 +0530

    Created modules

---
+++ script.js
@@ -0,0 +1,15 @@
+var budgetController = (function(){
+    console.log("BudgetCntl");
+})();
+
+
+var uiContoller = (function(){
+    console.log("UIcntm");
+})();
+
+
+var appController = (function(){
+    console.log("App");
+})();
+
+
